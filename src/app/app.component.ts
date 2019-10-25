f5825f442b145167a3a79246661ef5f88da55ce3
ramjam1215 <jimmy.c.corcoran@gmail.com>
2019-10-24 19:05:43 -0500

    todo-app started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AppService } from './app.service';
+import { Observable } from 'rxjs/';
+import { map } from 'rxjs/operators';
+import { config } from './app.config';
+import { Task } from './app.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+    
   title = 'todo-app';
+  tasks: Observable<any[]>;
+  myTask: string;
+  editMode: boolean = false;
+  taskToEdit: any = {};
+
+  constructor(private db: AngularFirestore, private taskService: AppService) { }
+
+  //had to add the pipe around the map function call, issue in tutorial
+  ngOnInit(): void {
+
+    this.tasks = this.db.collection(config.collection_endpoint)
+      .snapshotChanges()
+
+      .pipe(map(actions => {
+
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Task;
+
+          const id = a.payload.doc.id;
+
+          return { id, ...data };
+        });
+
+      })
+
+    );
+
+  }
+
+  edit(task) {
+    console.log(task);
+
+    this.taskToEdit = task;
+    this.editMode = true;
+    this.myTask = task.description;
+
+  }
+
+  saveTask() {
+    if (this.myTask !== null) {
+      let task = { description: this.myTask };
+      if (!this.editMode) {
+
+        console.log(task);
+        this.taskService.addTask(task);
+      }
+
+      else {
+
+        let taskId = this.taskToEdit.id;
+        this.taskService.updateTask(taskId, task);
+
+      }
+
+      this.editMode = false;
+      this.myTask = "";    }
+
+  }
+
+  deleteTask(task) {
+
+    let taskId = task.id;
+    this.taskService.deleteTask(taskId);
+
+  }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,15 +2,49 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+//ASIDE: i found these in the project overview settings,
+//under the firebase SDK snippet
+//also had the appID, and measurementId that isnt in the tutorial
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+
+    apiKey: 'AIzaSyD8qga2PVcdoBp78GF2WUxgx3VRIGRg8iE',
+    authDomain: 'todo-app-b02a7.firebaseapp.com',
+    databaseURL: 'https://todo-app-b02a7.firebaseio.com',
+    projectId: 'todo-app-b02a7',
+    storageBucket: 'todo-app-b02a7.appspot.com',
+    messagingSenderId: '90788126121',
+    appId: '1:90788126121:web:aa80864bf512a2c69bf8b8',
+    measurementId: 'G-TJ7ZGZKR33'
+  }
 };
 
+
 /*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+ *<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.2.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD8qga2PVcdoBp78GF2WUxgx3VRIGRg8iE",
+    authDomain: "todo-app-b02a7.firebaseapp.com",
+    databaseURL: "https://todo-app-b02a7.firebaseio.com",
+    projectId: "todo-app-b02a7",
+    storageBucket: "todo-app-b02a7.appspot.com",
+    messagingSenderId: "90788126121",
+    appId: "1:90788126121:web:aa80864bf512a2c69bf8b8",
+    measurementId: "G-TJ7ZGZKR33"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+ **/
+
